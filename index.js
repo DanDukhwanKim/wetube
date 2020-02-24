c653c5ad121e45202cca46952451f6c90bc9319b
DanDukhwanKim <pluvios@pluvios.com>
2020-02-25 02:56:10 +0900

    shortly reviewed and a progress

--- index.js
+++ index.js
@@ -7,4 +7,19 @@ function handleListening(){
     console.log(`Listening on: http://localhost:${PORT}`);
 }
 
+function handleHome(req, res){
+    console.log(req);
+    res.send("hello from home");
+
+}
+
+function handleProfile(req, res){
+    res.send("You are on my profile");
+
+}
+
+app.get("/", handleHome); 
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT,handleListening);
